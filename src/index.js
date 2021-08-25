b2ed09d3b9ca08ce489089192796658d5c558fbe
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-08-25 14:30:56 -0300

    save

--- src/index.js
+++ src/index.js
@@ -1,38 +1,58 @@
-const { request, response } = require('express');
-const express = require('express');
+app.use(logRequest)
+app.use('/projects/:id', validateProjectId)
 
-const app = express();
+app.get("/projects", (request, response) => {
+  const { title } = request.query
 
-app.get('/projects', (request, response) =>{
- return response.json([
-  'projeto 1',
-  'projeto 2',
- ])
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects
+
+  return response.json(results)
+});
+
+app.post("/projects", (request, response) => {
+  const { title, owner } = request.body
+
+  const project = { id: uuid(), title, owner }
+
+  projects.push(project)
+  return response.json(project)
 })
 
-app.post('/projects', (request, response) =>{
- return response.json([
-  'projeto 1',
-  'projeto 2',
-  'projeto 3',
- ])
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params
+  const { title, owner } = request.body
+
+  const projectIndex = projects.findIndex(project => project.id == id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' })
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  }
+  projects[projectIndex] = project
+
+  return response.json(project)
 })
 
-app.put('/projects/:id', (request, response) =>{
- return response.json([
-  'projeto 4',
-  'projeto 2',
-  'projeto 3'
- ])
-})
+app.delete("/projects/:id", (request, response) => {
+  const { id } = request.params
+  const projectIndex = projects.findIndex(project => project.id == id)
 
-app.delete('/projects', (request, response) =>{
- return response.json([
-  'projeto 2',
-  'projeto 3',
- ])
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
 })
 
 app.listen(3333, () => {
- console.log('🔐pai ta oon💻')
+  console.log("🔐pai ta oon e atualizado💻")
 })
